701542e7b535074097469f4b253fff9518c5c293
Zoe Christ <zoe@christ.de>
2020-07-08 16:02:55 +0200

    window GUI

--- coding_week/test.py
+++ coding_week/test.py
@@ -18,3 +18,19 @@ from MyPackage import power, average, SayHello
 SayHello()
 x=power(3,2)
 print("power(3,2) : ", x)
+
+from tkinter import *
+window=Tk()
+# add widgets here
+
+window.title('Hello Python')
+window.geometry("300x200+10+20")
+window.mainloop()
+
+from tkinter import *
+window=Tk()
+btn=Button(window, text="This is Button widget", fg='blue')
+btn.place(x=80, y=100)
+window.title('Hello Python')
+window.geometry("300x200+10+10")
+window.mainloop()
