9e8ebc5180f2e8380868c913d2d5893cc4bfa430
Zoe Christ <zoe@christ.de>
2020-07-08 09:43:57 +0200

    Eingabe Programm

--- coding_week/test.py
+++ coding_week/test.py
@@ -1,1 +1,8 @@
-print("Warum werd ich nicht angezeigt???")
+﻿price=int(input("Enter price: "))
+qty=int(input("Enter quantity: "))
+amt=price*qty
+if amt>1000:
+    print("discount applicable")
+    discount=amt*10/100
+    amt=amt-discount
+    print("Amount payable: ", amt)
