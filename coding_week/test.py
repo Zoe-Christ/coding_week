bf551bbcda2cdf5f55afa7f54f2c9a57c8fd39d5
Zoe Christ <zoe@christ.de>
2020-07-08 10:35:40 +0200

    square

--- coding_week/test.py
+++ coding_week/test.py
@@ -1,8 +1,15 @@
-﻿price=int(input("Enter price: "))
-qty=int(input("Enter quantity: "))
-amt=price*qty
-if amt>1000:
-    print("discount applicable")
-    discount=amt*10/100
-    amt=amt-discount
-    print("Amount payable: ", amt)
+﻿def math():
+    price=int(input("Enter price: "))
+    qty=int(input("Enter quantity: "))
+    amt=price*qty
+    if amt>1000:
+        print("discount applicable")
+        discount=amt*10/100
+        amt=amt-discount
+        print("Amount payable: ", amt)
+    return
+
+def square(x):
+    return (x*x)
+
+print("Quadrat: ", square(4))
