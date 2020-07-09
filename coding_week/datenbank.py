ec4801b8be61f412ae89914030bef2b4f162e2e7
Zoe Christ <zoe@christ.de>
2020-07-09 14:33:46 +0200

    Datenbank funktionsfähig

--- coding_week/datenbank.py
+++ coding_week/datenbank.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+# Datenbank erstellen
+
 """
 try:
     sqliteConnection = sqlite3.connect('SQLite_Python.db')
@@ -21,14 +23,16 @@ finally:
 
 """
 
+# Tabelle erstellen
+
+
+"""
 try:
     sqliteConnection = sqlite3.connect('SQLite_Python.db')
-    sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
+    sqlite_create_table_query = '''CREATE TABLE testTable (
                                 id INTEGER PRIMARY KEY,
                                 name TEXT NOT NULL,
-                                email text NOT NULL UNIQUE,
-                                joining_date datetime,
-                                salary REAL NOT NULL);'''
+                                vorname text NOT NULL);'''
 
     cursor = sqliteConnection.cursor()
     print("Successfully Connected to SQLite")
@@ -44,3 +48,50 @@ finally:
     if (sqliteConnection):
         sqliteConnection.close()
         print("sqlite connection is closed")
+
+"""
+
+# Datensatz einfügen
+"""
+
+db=sqlite3.connect('SQLite_Python.db')
+qry="insert into testTable (id, name, vorname) values(3, 'Dieterich', 'Antonia');"
+try:
+    cur=db.cursor()
+    cur.execute(qry)
+    db.commit()
+    print ("one record added successfully")
+except:
+    print ("error in operation")
+    db.rollback()
+db.close()
+"""
+
+# Datensätze ausgeben 
+"""
+db=sqlite3.connect('SQLite_Python.db')
+sql="SELECT * from testTable;"
+cur=db.cursor()
+cur.execute(sql)
+while True:
+    record=cur.fetchone()
+    if record==None:
+        break
+    print (record)
+db.close()
+"""
+# Datensatz löschen
+
+"""
+db=sqlite3.connect('SQLite_Python.db')
+qry="DELETE from testTable where id=3;"
+try:
+    cur=db.cursor()
+    cur.execute(qry)
+    db.commit()
+    print("record deleted successfully")
+except:
+    print("error in operation")
+    db.rollback()
+db.close()
+"""
