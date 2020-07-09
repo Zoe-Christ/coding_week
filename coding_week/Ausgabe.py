69c01d493805dc30e5dbf959767517d627e756bb
Zoe Christ <zoe@christ.de>
2020-07-09 15:33:27 +0200

    Datenbankbefehle funktionsfähig in Dokument Ausgabe

---
+++ coding_week/Ausgabe.py
@@ -0,0 +1,9 @@
+import datenbank
+
+#Beispielanwendung der Datenbank Funktionen 
+
+datenbank.ausgeben()
+
+datenbank.insert(4, 'Schierding', 'Jonah')
+
+#datenbank.delete(3)
--- coding_week/datenbank.py
+++ coding_week/datenbank.py
@@ -25,73 +25,81 @@ finally:
 
 # Tabelle erstellen
 
-
 """
-try:
-    sqliteConnection = sqlite3.connect('SQLite_Python.db')
-    sqlite_create_table_query = '''CREATE TABLE testTable (
-                                id INTEGER PRIMARY KEY,
-                                name TEXT NOT NULL,
-                                vorname text NOT NULL);'''
+    try:
+        sqliteConnection = sqlite3.connect('SQLite_Python.db')
+        sqlite_create_table_query = '''CREATE TABLE testTable (
+                                    id INTEGER PRIMARY KEY,
+                                    name TEXT NOT NULL,
+                                    vorname text NOT NULL);'''
 
-    cursor = sqliteConnection.cursor()
-    print("Successfully Connected to SQLite")
-    cursor.execute(sqlite_create_table_query)
-    sqliteConnection.commit()
-    print("SQLite table created")
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
+        cursor.execute(sqlite_create_table_query)
+        sqliteConnection.commit()
+        print("SQLite table created")
 
-    cursor.close()
+        cursor.close()
 
-except sqlite3.Error as error:
-    print("Error while creating a sqlite table", error)
-finally:
-    if (sqliteConnection):
-        sqliteConnection.close()
-        print("sqlite connection is closed")
+    except sqlite3.Error as error:
+        print("Error while creating a sqlite table", error)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
+            print("sqlite connection is closed")
+  """
 
-"""
+
 
 # Datensatz einfügen
-"""
 
-db=sqlite3.connect('SQLite_Python.db')
-qry="insert into testTable (id, name, vorname) values(3, 'Dieterich', 'Antonia');"
-try:
-    cur=db.cursor()
-    cur.execute(qry)
-    db.commit()
-    print ("one record added successfully")
-except:
-    print ("error in operation")
-    db.rollback()
-db.close()
-"""
+def insert(id, name, vorname):
+    db=sqlite3.connect('SQLite_Python.db')
+    qry="""insert into testTable (id, name, vorname) values(?,?,?);"""
+    try:
+        cur=db.cursor()
+        recordTuple = (id, name, vorname)
+        cur.execute(qry, recordTuple)
+        db.commit()
+        print ("one record added successfully")
+    except:
+        print ("error in operation")
+        db.rollback()
+    db.close()
+    return
+
 
 # Datensätze ausgeben 
-"""
-db=sqlite3.connect('SQLite_Python.db')
-sql="SELECT * from testTable;"
-cur=db.cursor()
-cur.execute(sql)
-while True:
-    record=cur.fetchone()
-    if record==None:
-        break
-    print (record)
-db.close()
-"""
+
+def ausgeben():
+    db=sqlite3.connect('SQLite_Python.db')
+    sql="SELECT * from testTable;"
+    cur=db.cursor()
+    cur.execute(sql)
+    while True:
+        record=cur.fetchone()
+        if record==None:
+            break
+        print (record)
+    db.close()
+    return
+  
+
 # Datensatz löschen
 
-"""
-db=sqlite3.connect('SQLite_Python.db')
-qry="DELETE from testTable where id=3;"
-try:
-    cur=db.cursor()
-    cur.execute(qry)
-    db.commit()
-    print("record deleted successfully")
-except:
-    print("error in operation")
-    db.rollback()
-db.close()
-"""
+
+def delete(id):
+    db=sqlite3.connect('SQLite_Python.db')
+    qry="""DELETE from testTable where id= ?;"""
+    try:
+        cur=db.cursor()
+        
+        cur.execute(qry, (id, ))
+        db.commit()
+        print("record deleted successfully")
+    except:
+        print("error in operation")
+        db.rollback()
+    db.close()
+    return
+
