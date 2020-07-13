32f0b1de54e3b05a52d13a6bff8ee38a89450239
Antonia Dieterich <a.dieterich27@gmail.com>
2020-07-13 11:20:33 +0200

    Kamera Klasse geschrieben für Anbindung an GUIund Raspberry Pi

---
+++ coding_week/raspCam.py
@@ -0,0 +1,37 @@
+class raspCam:
+    """description of class"""
+    from picamera import PiCamera
+    from time import sleep
+    import datenbank
+    import face_recognition
+    import os
+
+    def __init__(self): 
+        camera = PiCamera()
+        camera.start_preview
+
+    def takePicture ():
+        sleep(5)
+        camera.capture('/home/pi/Desktop/unknownPerson.jpg')
+
+    def recognize ():
+        camera.takePicture()
+
+        pic = '/home/pi/Desktop/unknownPerson.jpg' #ggf. Anführungszeichen statt Apostrophe
+        unknown_image = face_recognition.load_image_file(pic)
+        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+        known_faces = datenbank.ausgeben()
+        face_names = datenbank.giveName()
+        results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+        print(results)
+
+        #Bild von Raspbery löschen
+        os.remove('/home/pi/Desktop/unknownPerson.jpg')
+
+        #Namen zurückgeben
+        index = results.index(True)
+        if(index != null):
+            return face_names[index]
+        else:
+            return "I'm sorry, I don't know you yet. It's a pleasure to meet you though! Hi, I'm CowIT 18 :)"
+       
--- coding_week/test.py
+++ coding_week/test.py
@@ -1,31 +1,5 @@
-﻿def math():
-    price=int(input("Enter price: "))
-    qty=int(input("Enter quantity: "))
-    amt=price*qty
-    if amt>1000:
-        print("discount applicable")
-        discount=amt*10/100
-        amt=amt-discount
-        print("Amount payable: ", amt)
-    return
+﻿import raspCam
 
-def square(x):
-    return (x*x)
+cam = raspCam()
 
-print("Quadrat: ", square(4))
-
-from MyPackage import power, average, SayHello
-SayHello()
-x=power(3,2)
-print("power(3,2) : ", x)
-
-
-from tkinter import *
-window=Tk()
-lbl=Label(window, text="This is the Coding Week Window", fg='red', font=("Helvetica", 10))
-lbl.place(x=60, y=50)
-btn=Button(window, text="Press the Button", fg='blue')
-btn.place(x=80, y=100)
-window.title('Coding Week')
-window.geometry("300x200+10+10")
-window.mainloop()
+cam.takePicture()
