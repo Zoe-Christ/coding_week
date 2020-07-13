01aaff7367cf1346495eece9972523827ff6a3e1
Zoe Christ <zoe@christ.de>
2020-07-13 11:16:25 +0200

    Bilder in DB einfügen

--- coding_week/Ausgabe.py
+++ coding_week/Ausgabe.py
@@ -6,20 +6,18 @@ import face_recognition
 #datenbank.delete(3)
 
 #Datensatz einfügen
-
-#datenbank.insert((datenbank.givemaxID()+1), "basti_1.jpg")
+#datenbank.insert((datenbank.givemaxID()+1), "Zoe.jpeg")
 
 #Datensätze ausgeben
 
-"""
-pic = "basti_1.jpg"
-unknown_image = face_recognition.load_image_file(pic)
-unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-known_faces = datenbank.ausgeben()
-results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
-print(results)
+#pic = "basti_1.jpg"
+#unknown_image = face_recognition.load_image_file(pic)
+#unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+#known_faces = datenbank.ausgeben()
+#results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+#print(results)
 
-"""
+
 
 #datenbank.deleteTable()
 
