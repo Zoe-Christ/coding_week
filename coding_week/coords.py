91606ec44d28bc7cc925b6aab4be8f6894d4735f
Maurice Geisen <mauricegeisen@Maurices-MacBook-Pro.local>
2020-07-15 11:02:05 +0200

    Coords Funktionsupdate

--- coding_week/coords.py
+++ coding_week/coords.py
@@ -1,9 +1,9 @@
 import face_recognition as fr
 
-standard = fr.load_image_file("/Users/mauricegeisen/Documents/coding_week/coding_week/trump_2.jpg")
+standard = fr.load_image_file("/Users/mauricegeisen/Documents/coding_week/coding_week/Maurice_.jpeg")
 lm1 = fr.face_landmarks(standard)
 
-flex = fr.load_image_file("/Users/mauricegeisen/Documents/coding_week/coding_week/trump_1.jpg")
+flex = fr.load_image_file("/Users/mauricegeisen/Documents/coding_week/coding_week/Maurice_oB.jpeg")
 lm2 = fr.face_landmarks(flex)
 
 xs1 = []
@@ -13,17 +13,17 @@ xs2 = []
 ys2 = []
 
 for landmark1 in lm1:
-    for k, v in lm1.items():
+    for k, v in landmark1.items():
       xs1 += [x[0] for x in v]
       ys1 += [x[1] for x in v]
 
 for landmark2 in lm2:
-    for k, v in lm2.items():
+    for k, v in landmark2.items():
         xs2 += [x[0] for x in v]
         ys2 += [x[1] for x in v]
 
-durchschnitt    = xs1[55]-xs1[49]
-varWert         = xs2[55]-xs2[49]
+durchschnitt    = xs1[54]-xs1[48]
+varWert         = xs2[54]-xs2[48]
 
 print("Standard:", durchschnitt)
 print("Variable:", varWert)
